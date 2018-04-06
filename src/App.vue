5c2ea26226dd622b0bd168da64ef2aeb57f07c5d
James Armbrust <james.armbrust@gmail.com>
2018-04-06 05:48:15 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/" class="btn">Home</router-link>
+      <router-link to="/todolist" class="btn">Todo List</router-link>
+    </nav>
+    <div class="page-body">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+nav {
+  display: block;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is todolist page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
