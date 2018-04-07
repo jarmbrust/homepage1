56001e7306d3f7c6f8883e473e17631baa9a051a
James Armbrust <james.armbrust@gmail.com>
2018-04-07 13:39:18 -0600

    image loading

--- src/components/PetPics.vue
+++ src/components/PetPics.vue
@@ -2,17 +2,18 @@
   <div>
     <h1 class="heading">Glorious pet pictures will be displayed here!</h1>
      <b-card title="Muppet"
-            img-src="require('@/../../assets/Muppet1.png)"
+            img-src="~@/assets/Muppet1.png"
             img-alt="Muppet the dog"
             img-top
             tag="article"
             style="max-width: 200px;"
-    >
+      >
       <p class="card-text">
         Misc programs will be added here for your fun and excitement.  Until then, enjoy the veggies.
       </p>
       <b-button disabled variant="primary">Go somewhere</b-button>
     </b-card>
+
   </div>
 </template>
 
