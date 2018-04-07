dfdaae73145354e0146cba3a3fd94c955533989e
James Armbrust <james.armbrust@gmail.com>
2018-04-07 14:02:51 -0600

    another image

--- src/components/PetPics.vue
+++ src/components/PetPics.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <h1 class="heading">Glorious pet pictures will be displayed here!</h1>
-     <b-card title="Muppet"
+     <b-card class="pet-card"
+            title="Muppet"
             img-src="~@/assets/Muppet1.png"
             img-alt="Muppet the dog"
             img-top
             tag="article"
-            style="max-width: 200px;"
+            style="max-width: 250px;"
       >
       <p class="card-text">
-        Misc programs will be added here for your fun and excitement.  Until then, enjoy the veggies.
+        Adopted from the pound about 2 years ago, this Chinese crested/terrier mix loves
+        barking, sleeping, and playing with his toys.
       </p>
-      <b-button disabled variant="primary">Go somewhere</b-button>
+    </b-card>
+    <b-card class="pet-card"
+            title="Toshi"
+            img-src="~@/assets/Toshi1.png"
+            img-alt="Toshi the cat"
+            img-top
+            tag="article"
+            style="max-width: 250px;"
+      >
+      <p class="card-text">
+        Adopted almost 10 years ago, Toshi loved warmth, sunbeams, and just chill'n.  He passed away 4/5/2018
+        at the aprox age of 16.</p>
     </b-card>
 
   </div>
@@ -26,5 +39,10 @@ export default {
 <style>
   .heading {
     font-size: 20px;
+    margin: 20px;
+  }
+  .pet-card {
+    float: left;
+    margin: 10px;
   }
 </style>
