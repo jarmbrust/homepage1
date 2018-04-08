54c72793a621324ebc1195cbf35ce38b830c7fc5
James Armbrust <james.armbrust@gmail.com>
2018-04-08 11:09:13 -0600

    nav styles

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container>
+  <b-container role="navigation" class="nav-container">
     <b-nav vertical pills>
       <router-link to="/" class="nav-link">Home</router-link>
       <router-link to="/miscprojects" class="nav-link">Misc Projects</router-link>
@@ -16,5 +16,11 @@ export default {
 </script>
 
 <style>
-
+  .nav-container {
+    border: 2px solid #000;
+    padding: 20px;
+    border-bottom-right-radius: 25px;
+    background-color: #bec4ce;
+    box-shadow: 2px 3px #7f7f80;
+  }
 </style>
