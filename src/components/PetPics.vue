7909d2f727188c154e5fe67e852b84a10faf4ea7
James Armbrust <james.armbrust@gmail.com>
2018-04-08 06:06:06 -0600

    the rest of the animals

--- src/components/PetPics.vue
+++ src/components/PetPics.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <h1 class="heading">Glorious pet pictures will be displayed here!</h1>
-     <b-card class="pet-card"
+    <h1 class="heading">Meet the members of our family!</h1>
+    <b-card class="pet-card"
+            title="Chewie"
+            img-src="~@/assets/Chewie1.png"
+            img-alt="Chewie the dog"
+            img-top
+            tag="article"
+            style="max-width: 250px;"
+      >
+      <p class="card-text">
+        Adopted from the pound about 7 years ago, this lovable black lab loves
+        lounging on the couch, walks, and generally ignoring physics.
+      </p>
+    </b-card>
+    <b-card class="pet-card"
             title="Muppet"
             img-src="~@/assets/Muppet1.png"
             img-alt="Muppet the dog"
@@ -11,7 +24,46 @@
       >
       <p class="card-text">
         Adopted from the pound about 2 years ago, this Chinese crested/terrier mix loves
-        barking, sleeping, and playing with his toys.
+        barking, looking stylish, and playing with his toys.
+      </p>
+    </b-card>
+    <b-card class="pet-card"
+            title="Lexi"
+            img-src="~@/assets/Lexi1.png"
+            img-alt="Lexi the dog"
+            img-top
+            tag="article"
+            style="max-width: 250px;"
+      >
+      <p class="card-text">
+        Adopted from the pound about 3 years ago, this chihuahua mix loves
+        cuddling, licking faces, and occasional bezerker rages.
+      </p>
+    </b-card>
+    <b-card class="pet-card"
+            title="Stormy"
+            img-src="~@/assets/Stormy1.png"
+            img-alt="Stormy the dog"
+            img-top
+            tag="article"
+            style="max-width: 250px;"
+      >
+      <p class="card-text">
+        Adopted from the pound about 5 years ago, this chihuahua mix loves
+        sleeping, burrowing under blankets, and more sleeping.
+      </p>
+    </b-card>
+    <b-card class="pet-card"
+            title="Kitten"
+            img-src="~@/assets/Kitten1.png"
+            img-alt="Kitten the cat"
+            img-top
+            tag="article"
+            style="max-width: 250px;"
+      >
+      <p class="card-text">
+        Adopted around 8 years ago, "Kitten" loves attention, being the center of attention,
+        and finding interesting places to take a nap.
       </p>
     </b-card>
     <b-card class="pet-card"
@@ -24,7 +76,8 @@
       >
       <p class="card-text">
         Adopted almost 10 years ago, Toshi loved warmth, sunbeams, and just chill'n.  He passed away 4/4/2018
-        at the aprox age of 16.</p>
+        at the aprox age of 16.
+      </p>
     </b-card>
 
   </div>
