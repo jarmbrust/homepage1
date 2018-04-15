f10ceaaec522015cfbc2f9e0eef8d39dd6a39b91
James Armbrust <james.armbrust@gmail.com>
2018-04-15 09:09:24 -0600

    ugg, typo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,7 +7,7 @@
       <p>
         This site is pretty bare-bones at the moment. I started playing around with making it 4/6/2018 and hope
         to have it more or less fleshed out by the end of April.  Right now I am using it to host a few
-        peices of code I wrote and projects I am working on, and to of course show off pictures of my dogs.
+        peices of code I wrote and projects I am working on, and of course show off pictures of my dogs.
       </p>
   </div>
 </template>
